15b8a52112edff3bee3615684c945f29c274ac39
Emiliano Alegre <emiliano@creativedog.agency>
2022-11-03 19:06:04 -0300

    path define

--- index.js
+++ index.js
@@ -6,21 +6,16 @@ const cors = require("cors")
 const Router = require("./routes/routes")
 const passport = require("passport")
 const app = express()
-
-const PORT = process.env.PORT || 4000
+const path = require("path")
 
 app.use(cors())
 app.use(express.json())
 app.use(passport.initialize())
 app.use("/api", Router)
 
-
 app.use(express.static("frontend/build"))
 app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+  res.status(200).sendFile(path.join(__dirname + "/frontend/build/index.html"))
 })
 
-app.listen(PORT, () =>
-    console.log("App listening on port " + PORT
-)
-)
+app.listen(4000, () => console.log("App listening on port " + 4000))
