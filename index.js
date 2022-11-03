0efb0f5f1ea81c0c6d76bf51cd31f9155fbd399c
Emiliano Alegre <emiliano@creativedog.agency>
2022-11-03 19:10:16 -0300

    asd

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(passport.initialize())
 app.use("/api", Router)
 
 app.use(express.static("frontend/build"))
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.status(200).sendFile(path.join(__dirname + "/frontend/build/index.html"))
 })
 
